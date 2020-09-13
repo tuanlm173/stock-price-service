6d79a4866a2ed9a13abb3916321f4b4f580531d1
tuanlm173 <tuanlm.cca@gmail.com>
2020-09-13 16:38:45 +0700

    Add gitignore and config

--- src/main/java/com/tuanle/stockintelligence/StockPriceApplication.java
+++ src/main/java/com/tuanle/stockintelligence/StockPriceApplication.java
@@ -3,16 +3,30 @@ package com.tuanle.stockintelligence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
 @EnableCaching
-
+@EnableAsync
 public class StockPriceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StockPriceApplication.class, args);
     }
 
-
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("StockpriceLookup-");
+        executor.initialize();
+        return executor;
+    }
 
 }
